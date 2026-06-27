897149b27ba48c69132a17c9a82e8f7ce50d3434
irena613 <rena.latinovska@gmail.com>
2026-06-27 16:06:46 +0200

    Adding key collection logic as well as a key spawner logic that will randomly add key throughout the map

---
+++ tresure-hunting-hound/key.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+var coinsCollected = 0
+var keysCollected = 0
+const keysRequired = 7
+var player_in_chest_area = false
+@onready var timer = $Timer
+func _process(_delta: float) -> void:
+	rotate_y(0.02)
+	
+func _update_label() -> void:
+	$Label.text = "Keys: " + str(keysCollected) + "/" + str(keysRequired) + "   Coins: " + str(coinsCollected)
+
+func _on_collect_zone_body_entered(body: Node3D) -> void:
+	if body.name == "Doggo":
+		var score = get_tree().get_root().get_node("Node3D/CanvasLayer")
+		queue_free()
+
+func _on_danger_zone_body_entered(body: Node3D) -> void:
+	print("entered danger zone: ", body.name)
+	if body.name == "Doggo":
+		$DangerTimer.start()
+
+func _on_danger_zone_body_exited(body: Node3D) -> void:
+	if body.name == "Doggo":
+		$DangerTimer.stop()
+
+func _on_danger_timer_timeout() -> void:
+	var score = get_tree().get_root().get_node("Node3D/CanvasLayer")
+	get_tree().get_root().get_node("Node3D/KeySpawner").respawn_key()
+	queue_free()
---
+++ tresure-hunting-hound/key_spawner.gd
@@ -0,0 +1,38 @@
+extends Node3D
+
+const KEY_SCENE = preload("res://key.tscn")
+const KEY_COUNT = 7
+const SPAWN_Y = 2.0
+const MIN_SPACING = 35.0
+
+const MAP_MIN = Vector2(-105.0, -98.0)
+const MAP_MAX = Vector2(110.0, 116.0)
+
+const EXCLUSIONS = [
+	[100.0, -81.4, 35.0],
+	[88.5,  -63.3, 15.0],
+	[0.0,   0.0,   15.0],
+]
+
+func _ready() -> void:
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var placed: Array[Vector3] = []
+	var attempts := 0
+	while placed.size() < KEY_COUNT and attempts < 500:
+		attempts += 1
+		var x = rng.randf_range(MAP_MIN.x, MAP_MAX.x)
+		var z = rng.randf_range(MAP_MIN.y, MAP_MAX.y)
+		var pos = Vector3(x, SPAWN_Y, z)
+		if _in_exclusion(x, z):
+			continue
+		var key = KEY_SCENE.instantiate()
+		key.position = pos
+		add_child(key)
+		placed.append(pos)
+
+func _in_exclusion(x: float, z: float) -> bool:
+	for e in EXCLUSIONS:
+		if Vector2(x, z).distance_to(Vector2(e[0], e[1])) < e[2]:
+			return true
+	return false
